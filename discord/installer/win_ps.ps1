a363e6b74045064164a5f47101903b1139568ee8
ByzantineFailure <byzanitnefailure@gmail.com>
2016-01-03 13:55:25 -0800

    Discord-specific changes added

---
+++ discord/installer/win_ps.ps1
@@ -0,0 +1,48 @@
+$node_url = "https://nodejs.org/dist/v4.2.4/win-x86/node.exe"
+$node_output = "./node.exe"
+
+function Download-Node
+{
+    Write-Host "The installer could not find a 4.2.x install of Node.js.  This is needed to"
+    Write-Host "run the install script.  Is it okay to download a standalone version of"
+    Write-Host "Node.js v4.2.4 (about 11 MB) to run the install script then delete it right"
+    Write-Host "after it runs? (Installer will quit without installing if it is not)"
+    Write-Host "Press Y if it is okay (any other key if not)"
+    $key = [Console]::ReadKey($true)
+    if($key.Key -eq 'y') {
+        Write-Host "Downloading Node.js v4.2.4 standalone binary..."
+        $wc = New-Object System.Net.WebClient
+        $wc.DownloadFile($node_url, $node_output)
+    } else {
+        Write-Host "Input was not 'y' or 'Y', quitting without installing BPM..."
+        exit
+    }
+}
+
+if (Get-Command node.exe -errorAction SilentlyContinue)
+{
+    Write-Host "Found node on path, checking version..."
+    $node_version = (node --version) | Out-String
+    $node_version = $node_version.trim()
+    if ($node_version -like "*v4.2*") {
+        Write-Host "Version is v4.2.x, running installer..."
+        node.exe index.js
+    }
+    else
+    {
+        Write-Host "Found version $node_version, not a v4.2.x."
+        Download-Node
+        & $node_output index.js
+        Remove-Item $node_output
+    }
+}
+else
+{
+    Write-Host "Cannot find node on PATH"
+    Download-Node
+    Write-Host "Downloaded node binary, running installer..."
+    & $node_output index.js
+    Write-Host "Removing downloaded node binary..."
+    Remove-Item $node_output
+}
+
