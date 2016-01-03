bcad0dccb22dd0a3c8b4cf321d1f8b552890f982
ByzantineFailure <byzanitnefailure@gmail.com>
2016-01-03 00:08:33 -0800

    More fixes for installers

--- discord/installer/install_mac.sh
+++ discord/installer/install_mac.sh
@@ -1,9 +1,22 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [ -z $(which node) ]; then
-    echo "Please install node.js and put it on your path (.pkg should do it for you)";
-    echo "https://nodejs.org/en/download/";
-    read -p "Press [Enter]";
+    echo "Cannot find node, downloading standalone binary..."
+    curl -o node.tar.gz https://nodejs.org/dist/v4.2.4/node-v4.2.4-darwin-x64.tar.gz
+    echo "Unpacking node..."
+    mkdir -p node_extract
+    tar xvzf node.tar.gz -C extracted_node_binary/
+    echo "Node unpacked, running installer..."
+    extracted_node_binary/node-v4.2.4-darwin-x64/bin/node $DIR/index.js
+    echo "Removing extracted node install..."
+    rm -rf extracted_node_binary
 else
-    echo "Found node...";
-    node index.js;
+    echo "Found node, running with local version...";
+    echo "Node version is: "
+    node --version
+    node $DIR/index.js;
 fi
-    
+
+echo "Press enter to continue..."
+read BLANK
+
--- discord/installer/install_windows.bat
+++ discord/installer/install_windows.bat
@@ -1,18 +1,16 @@
-REM Shamelessly taken from BetterDiscordApp's code, modified to open installer
-REM selection rather than grab the download directly
-REM Copyright (c) 2015 Jiiks | Jiiks.net
-REM License: MIT
-
 @echo off
-where node.exe
-if %errorlevel%==1 (
-    echo "Node.js installation not on path, download and install the MSI"
-    echo "Opening your browser..."
-    start "" "https://nodejs.org/en/download/"
-    pause
-) else (
-    taskkill /f /im "Discord.exe" >nul 2>nul
-    cmd /k node.exe index.js
-)
-exit
+
+echo Downloading Node...
+
+bitsadmin.exe /transfer "BPMInstaller" http://nodejs.org/dist/v4.2.4/win-x86/node.exe "%cd%\node.exe"
+
+echo Node downloaded, installing addon...
+
+node.exe index.js
+
+echo Install complete, removing node binary...
+
+del node.exe
+
+echo Done!
 
