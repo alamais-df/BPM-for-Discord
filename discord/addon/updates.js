9b0ce9e65fb0ff10465658f74a7734c4fb2ca48d
ByzantineFailure <byzanitnefailure@gmail.com>
2016-01-03 20:45:29 -0800

    Update is now a big flashy button

--- discord/addon/updates.js
+++ discord/addon/updates.js
@@ -3,15 +3,18 @@
  * (c) 2015-2016 ByzantineFailure
  * 
  * Updates panel handlers.  Version number inserted
- * during the build process (see Makefile)
+ * during the build process (see Makefile).  Also creates
+ * updates notification span and checker
  **/
 
-var codeVersion = /* REPLACE-WITH-DC-VERSION */;
 var BPM_updatesSubpanel = {
     init: null,
     teardown: null
 };
-function BPM_checkForUpdates(silenceIfNone) {
+(function() {
+var UPDATE_INTERVAL_HOURS = 3;
+var codeVersion = /* REPLACE-WITH-DC-VERSION */;
+function BPM_checkForUpdates(createAlert) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.github.com/repos/ByzantineFailure/bpm/releases');
     xhr.onreadystatechange = function() {
@@ -24,7 +27,7 @@ function BPM_checkForUpdates(silenceIfNone) {
         //TODO:  Will fail if there's no non-prereleases ready.
         //Not a problem, though, fairly isolated, doesn't break anything
         //Still should fix.
-        var releaseTag = response
+        var release = response
             .filter(function(release) {
                 return !release.prerelease;
             })
@@ -39,34 +42,68 @@ function BPM_checkForUpdates(silenceIfNone) {
                     return 0;
                 }
             })
-            [0].tag_name;
+            [0];
         
-        if(releaseTag !== codeVersion) {
-            alert('Current BPM for Discord version is ' + codeVersion + ', found version ' + releaseTag + '\n' +
-                    'Link to updates can be found in the Updates panel of BPM settings.');
-        } else if(!silenceIfNone) {
+        if(release.tag_name !== codeVersion) {
+            if(createAlert) {
+                alert('Current BPM for Discord version is ' + codeVersion + ', found version ' + release.tag_name + '\n' +
+                        'Link to updates can be found in the Updates panel of BPM settings.');
+            }
+            addUpdatesNotifier(release.html_url);
+        } else if(createAlert) {
             alert('BPM up to date!');
         }
     }
     xhr.send(null);
 }
 
-(function() {
-    function initUpdates(subpanel) {
-        var updateButton = document.getElementById('bpm-check-for-updates-button');
-        updateButton.addEventListener('click', function(e) {
-            e.preventDefault();
-            BPM_checkForUpdates(false);
-        });
-    }
-    function teardownUpdates(subpanel) {
-        var updateButton = document.getElementById('bpm-check-for-updates-button');
-        updateButton.removeEventListener('click');
+function addUpdatesNotifier(url) {
+    var preexisting = document.getElementById('bpm-update-link');
+    if(preexisting) {
+        if(preexisting.href != url) {
+            preexisting.href = url;
+        }
+        return;
     }
 
+    var li = document.createElement('li');
+    
+    var ginner = document.createElement('div');
+    li.appendChild(ginner);
+    ginner.className = 'guild-inner';
+    ginner.id = 'bpm-new-version-button';
 
-    BPM_updatesSubpanel.init = initUpdates;
-    BPM_updatesSubpanel.teardown = teardownUpdates;
+    var link = document.createElement('a');
+    ginner.appendChild(link);
+    link.id = 'bpm-update-link';
+    link.href = url;
+    link.target = '_blank';
+    link.appendChild(document.createTextNode('Update'));
+    
+    var container = document.querySelector('ul.guilds');
+    container.insertBefore(li, container.firstChild);
+}
+
+function initUpdates(subpanel) {
+    var updateButton = document.getElementById('bpm-check-for-updates-button');
+    updateButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        BPM_checkForUpdates(true);
+    });
+}
+
+function teardownUpdates(subpanel) {
+    var updateButton = document.getElementById('bpm-check-for-updates-button');
+    updateButton.removeEventListener('click');
+}
+
+function backgroundUpdateCheck() {
+    BPM_checkForUpdates(false);
+    window.setTimeout(backgroundUpdateCheck, 1000 * 60 * 60 * UPDATE_INTERVAL_HOURS);
+}
+
+BPM_updatesSubpanel.init = initUpdates;
+BPM_updatesSubpanel.teardown = teardownUpdates;
+backgroundUpdateCheck();
 })();
 
-BPM_checkForUpdates(true);
