43d54ff63f6a86b4140458391a7e5b3f329c20bd
ByzantineFailure <byzanitnefailure@gmail.com>
2016-02-13 21:56:46 -0800

    Clean installer script a bit

--- discord/installer/constants.js
+++ discord/installer/constants.js
@@ -2,6 +2,7 @@
 module.exports = {
     bpmVersion: '^0.0.1',
     requireStatement: 'var bpm = require(\'dc-bpm\');',
-    injectStatement: 'var bpmInstance = new bpm(mainWindow);\nbpmInstance.init();'
+    injectStatement: 'var bpmInstance = new bpm(mainWindow);\nbpmInstance.init();',
+    injectLookFor: 'global.mainWindowId = mainWindow.id;'
 };
 
