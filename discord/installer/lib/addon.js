b703f48732f2b9cd5849217319ce89d54e908af4
ByzantineFailure <byzanitnefailure@gmail.com>
2016-02-27 11:30:27 -0800

    Adds ability to create and load custom scripts

--- discord/installer/lib/addon.js
+++ discord/installer/lib/addon.js
@@ -14,6 +14,7 @@ var fs = require('fs-extra'),
     path = require('path');
 
 function extractAddonCode(paths) {
+    backupOldCustom(paths);
     console.log('Removing previous bpm, if it exists...');
     if(fs.existsSync(paths.addonExtract)) {
         fs.removeSync(paths.addonExtract);
@@ -22,5 +23,20 @@ function extractAddonCode(paths) {
     console.log('Extracting bpm data to discord data...');
     asar.extractAll(paths.addonSource, paths.addonExtract); 
     console.log('Done extracting bpm data');
+    restoreOldCustom(paths);
 }
 
+function backupOldCustom(paths) {
+    console.log('Backing up old custom scripts to ' + paths.addonCustomBackup);
+    fs.ensureDirSync(paths.addonCustom); 
+    fs.copySync(paths.addonCustom, paths.addonCustomBackup);
+    console.log('Done backing up old custom scripts');
+}
+
+function restoreOldCustom(paths) {
+    console.log('Restoring old custom scripts');
+    fs.ensureDirSync(paths.addonCustomBackup);
+    fs.copySync(paths.addonCustomBackup, paths.addonCustom);
+    fs.removeSync(paths.addonCustomBackup);
+    console.log('Restored old custom scripts to ' + paths.addonCustom);
+}
