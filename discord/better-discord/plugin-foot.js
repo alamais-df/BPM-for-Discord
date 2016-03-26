4ddf1c2754897c4ae92251fc2fa7a1732769d25e
ByzantineFailure <byzanitnefailure@gmail.com>
2016-03-25 19:05:50 -0700

    Generate BetterDiscord plugin

---
+++ discord/better-discord/plugin-foot.js
@@ -0,0 +1,11 @@
+/* cat in bpm.js from addon */
+}
+bpm.prototype.unload = function() {};
+bpm.prototype.start = function() {};
+bpm.prototype.stop = function() {};
+bpm.prototype.getName = function() { return "BPM for BetterDiscord"; };
+bpm.prototype.getVersion = function() { return "0.1"; };
+bpm.prototype.getAuthor = function() { return "/u/Chalcedon"; };
+bpm.prototype.getDescription = function() { return "BPM in BetterDiscord.  DO NOT ENABLE/DISABLE."; };
+bpm.prototype.getSettingsPanel = function() { return "SEE BPM TAB"; };
+
