7f55cc80d00b184462f127716cf0867881c5f39c
ByzantineFailure <byzanitnefailure@gmail.com>
2016-07-13 20:57:20 -0700

    Move to 'start' BetterDiscord event to support new versions

--- discord/better-discord/plugin-foot.js
+++ discord/better-discord/plugin-foot.js
@@ -1,11 +1,11 @@
 /* cat in bpm.js from addon */
 }
 bpm.prototype.unload = function() {};
-bpm.prototype.start = function() {};
+bpm.prototype.load = function() {};
 bpm.prototype.stop = function() {};
 bpm.prototype.getName = function() { return "BPM for BetterDiscord"; };
 bpm.prototype.getVersion = function() { return "0.1"; };
 bpm.prototype.getAuthor = function() { return "/u/Chalcedon"; };
-bpm.prototype.getDescription = function() { return "BPM in BetterDiscord.  DO NOT ENABLE/DISABLE."; };
+bpm.prototype.getDescription = function() { return "BPM in BetterDiscord.  DO NOT RE-ENABLE AFTER INITIAL ENABLE"; };
 bpm.prototype.getSettingsPanel = function() { return "SEE BPM TAB"; };
 
