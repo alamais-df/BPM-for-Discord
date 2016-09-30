1dbbfab7f548629a6d8b18e43f374ecac073994e
ByzantineFailure <byzanitnefailure@gmail.com>
2016-09-30 13:58:55 -0700

    Add setting to move search button to top right (#56)
    * Adds a setting which moves the emote search button to the top-right
    * Re-ordered load sequence so that `search.js` is loaded after `core.js`
    
    Closes #56, addresses #55

--- discord/addon/search/search.js
+++ discord/addon/search/search.js
@@ -2,24 +2,74 @@
  * BPM for Discord
  * (c) 2015-2016 ByzantineFailure
  *
- * Adds the search button to the discord UI
+ * Adds the search button to the discord UI.  The
+ * listener which opens the window when the button
+ * is pressed is actually added by core.js -- so,
+ * we just create the button and maintain a reference
+ * to the node.
  **/
 require('!style!css!./search.css');
 
-var BPM_utils = require('../utils.js');
+var BPM_utils = require('../utils.js'),
+    searchButton;
 
-function modifyHelpContainer(container) {
-    container.className += ' bpm-help-container';
-    createSearchButton(container);
+// We rely on BPM's core code to attach this listener.
+// We also store this button in a module-global scope 
+// because we are bad people and cannot maintain a consistent
+// reference to it otherwise.
+function createSearchButton(prefs) {
+    var className = prefs.searchButtonTopRight ? 'header-toolbar' : 'guilds-wrapper';
+    BPM_utils.waitForElementByClass(className, function(container) {
+        var elementType = prefs.searchButtonTopRight ? 'button' : 'div';
+        
+        searchButton = document.createElement(elementType);
+        searchButton.className = 'bpm-emote-search-button' + 
+            (prefs.searchButtonTopRight ? '' : ' bpm-emote-search-button-bottom-left');
+        
+        container.appendChild(searchButton);
+        if(prefs.searchButtonTopRight) {
+            listenOnAppChange();
+        }
+    });
 }
 
-//We rely on BPM's core code to attach this listener
-function createSearchButton(container) {
-    var searchButton = document.createElement('div');
-    searchButton.className = 'bpm-emote-search-button';
-     
-    container.appendChild(searchButton);
+// The elements which contain the actual header and buttons
+// for the top-right search button are often removed and re-added
+// by discord's React code.  So, we need to wait for and React (hehe)
+// to those DOM events to re-add the button if it doesn't exist.
+// This listener is SUPER-noisy and presents an unfortunate amount
+// of CPU overhead, but there's no other choice unless we actually
+// hook into Discord's React code somehow (we cannot do that).
+function listenOnAppChange() {
+    var appDiv = document.getElementsByClassName('app')[0];
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(e) {
+            var headerToolbar = document.getElementsByClassName('header-toolbar')[0];
+            if(!headerToolbar) {
+                return;
+            }
+            
+            var button = document.getElementsByClassName('bpm-emote-search-button')[0];
+            var firstToolbarButton = headerToolbar.childNodes[0];
+            if(button) {
+                // Move the button to the farthest-left position if it already exists
+                // and isn't already there.
+                if (firstToolbarButton && firstToolbarButton !== button) {
+                    headerToolbar.insertBefore(searchButton, firstToolbarButton); 
+                }
+                return;
+            }
+            
+            if(!firstToolbarButton) {
+                headerToolbar.appendChild(searchButton);
+            } else {
+                headerToolbar.insertBefore(searchButton, firstToolbarButton); 
+            }
+        });
+    });
+
+    observer.observe(appDiv, { childList: true, subtree: true });
 }
 
-BPM_utils.waitForElementByClass('guilds-wrapper', modifyHelpContainer);
+BPM_utils.retrievePrefs(createSearchButton);
 
