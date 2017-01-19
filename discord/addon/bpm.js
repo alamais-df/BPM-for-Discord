7aa6c2e5eb6baf516cd82c2164346b1d10c0e339
ByzantineFailure <byzanitnefailure@gmail.com>
2017-01-18 22:50:56 -0800

    Fix BD storage shim

--- discord/addon/bpm.js
+++ discord/addon/bpm.js
@@ -1,14 +1,15 @@
-// Hack to get localStorage working again.  Maybe
-// Jiiks will get a better, electron-based way?
-if(window.localStorage === undefined) {
-    var iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    document.body.appendChild(iframe);
-    window.localStorage = iframe.contentWindow.localStorage;
-}
-
 // BetterDiscord pls.
-window.localStorage = bdPluginStorage || window.localStorage;
+try {
+    window.localStorage = bdPluginStorage;
+} catch (e) {
+    // Non-BD localStorage shim
+    if(window.localStorage === undefined) {
+        var iframe = document.createElement('iframe');
+        iframe.style.display = 'none';
+        document.body.appendChild(iframe);
+        window.localStorage = iframe.contentWindow.localStorage;
+    }
+}
 
 require('./updates/updates.js');
 require('./settings/settings.js');
